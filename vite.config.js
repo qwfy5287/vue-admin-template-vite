4181235d19b7aacce98dbeb2da5a5abd11759a9d
qwfy5287 <qwfy5287@qq.com>
2021-04-14 17:55:12 +0800

    feat: svg: svg-sprite-loader, https://segmentfault.com/a/1190000039255368

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,12 @@
 import { defineConfig } from 'vite'
-// import vue from "@vitejs/plugin-vue";
+
 const path = require('path')
 const { createVuePlugin } = require('vite-plugin-vue2')
+import { svgBuilder } from './src/plugins/svgBuilder'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    // vue()
-    createVuePlugin(),
-  ],
+  plugins: [createVuePlugin(), svgBuilder('./src/icons/svg/')],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
