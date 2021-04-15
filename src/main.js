71931f78eb962073f9730fe84ec3c9ae51a80998
qwfy5287 <qwfy5287@qq.com>
2021-04-15 14:19:45 +0800

    feat: @vue/composition-api

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import 'normalize.css/normalize.css' // A modern alternative to CSS resets
 
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import locale from 'element-ui/lib/locale/lang/en' // lang i18n
+// import locale from 'element-ui/lib/locale/lang/en' // lang i18n
 
 import '@/styles/index.scss' // global css
 
@@ -31,9 +31,16 @@ if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'developme
 }
 
 // set ElementUI lang to EN
-Vue.use(ElementUI, { locale })
+// Vue.use(ElementUI, { locale })
+Vue.use(ElementUI, { size: 'small' })
 // 如果想要中文版 element-ui，按如下方式声明
 // Vue.use(ElementUI)
+
+/**
+ * VueCompositionApi
+ */
+import VueCompositionApi from '@vue/composition-api'
+Vue.use(VueCompositionApi)
 
 Vue.config.productionTip = false
 
